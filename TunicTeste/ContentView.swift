5eef8fbffcec135ca15635521abb989d887ad4c0
ricardosvale <ricardost3@gmail.com>
2024-09-25 12:06:50 -0300

    obejtivos

--- TunicTeste/ContentView.swift
+++ TunicTeste/ContentView.swift
@@ -14,8 +14,9 @@ struct ContentView: View {
     @State private var arquivoAnexado: String? = nil
     
     var body: some View {
+        
         VStack(spacing: 20) {
-           
+            
             Text("Objetivo")
                 .font(.title)
                 .fontWeight(.bold)
@@ -26,34 +27,78 @@ struct ContentView: View {
                 .cornerRadius(8)
                 .foregroundColor(.white)
                 .padding(.horizontal)
-          
+            
             TextField("Insira o título do objetivo", text: $tituloObjetivo)
                 .padding()
                 .background(Color.green.opacity(0.7))
                 .cornerRadius(8)
                 .foregroundColor(.white)
                 .padding(.horizontal)
-        }
-        
-        HStack{
-            Text("Materias")
-                .font(.title3)
-                .padding(.leading)
-            Spacer()
-            ZStack{
-                RoundedRectangle(cornerRadius: 8)
-                    .fill(Color.green)
-                    .frame(width: 35, height: 30)
-                
-                Image(systemName: "plus.circle.fill")
-                    .foregroundColor(.black)
-                    .font(.title2)
-            }
             
+            
+            HStack{
+                Text("Materias")
+                    .font(.title3)
+                    .padding(.leading)
+                Spacer()
+                ZStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.green)
+                        .frame(width: 35, height: 30)
+                    
+                    Image(systemName: "plus.circle.fill")
+                        .foregroundColor(.black)
+                        .font(.title2)
+                }
+                .padding(.trailing)
+            }
+            .padding(.top)
+           
+            if let arquivo = arquivoAnexado{
+                HStack{
+                    Text(arquivo)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Image(systemName: "doc.on.doc")
+                        .foregroundColor(.black)
+                }
+                .padding()
+                .background(Color.purple.opacity(0.4))
+                .cornerRadius(8)
+                .padding(.horizontal)
+            } else {
+                HStack{
+                    Image(systemName: "paperclip")
+                        .foregroundColor(.black)
+                    Text("Anexar")
+                        .foregroundColor(.black)
+                    Spacer()
+                }
+                .padding()
+                .background(Color.purple.opacity(0.4))
+                .cornerRadius(8)
+                .padding(.horizontal)
+            
+            }
+            TextField("Tipo de arquivo (extensão, como: .pdf )", text: $tipoArquivo)
+                .padding(.horizontal)
+                
+            Button(action: {
+                           // Ação de adicionar objetivo
+                       }) {
+                           Text("Adicionar objetivo")
+                               .foregroundColor(.white)
+                               .frame(maxWidth: .infinity)
+                               .padding()
+                               .background(Color.cyan)
+                               .cornerRadius(8)
+                       }
+                       .padding(.horizontal)
+
+                       Spacer()
         }
-        .padding(.trailing)
         
-    }
+      }
       
 }
 
