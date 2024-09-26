c5d693d9a78e6607df00735f03267e527c5a8ee6
Ricardo Silva Vale <ricardosilvavale@MacBook-Air-de-Ricardo.local>
2024-09-26 01:05:39 -0300

    Obetivos

--- TunicTeste/ContentView.swift
+++ TunicTeste/ContentView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct ContentView: View {
     @State private var tituloObjetivo = ""
     @State private var descricaoObjetivo = ""
-    @State private var tipoArquivo = ""
     @State private var arquivoAnexado: String? = nil
     
     var body: some View {
@@ -28,7 +27,7 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .padding(.horizontal)
             
-            TextField("Insira o título do objetivo", text: $tituloObjetivo)
+            TextField("Insira a descrição do objetivo", text: $tituloObjetivo)
                 .padding()
                 .background(Color.green.opacity(0.7))
                 .cornerRadius(8)
@@ -40,72 +39,76 @@ struct ContentView: View {
                 Text("Materias")
                     .font(.title3)
                     .padding(.leading)
+                
                 Spacer()
-                ZStack{
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.green)
-                        .frame(width: 35, height: 30)
-                    
-                    Image(systemName: "plus.circle.fill")
-                        .foregroundColor(.black)
-                        .font(.title2)
-                }
-                .padding(.trailing)
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(Color.green)
+                    .frame(width: 35, height: 30)
+                    .overlay((Image(systemName: "plus.circle.fill")))
+                    .foregroundColor(.black)
+                    .font(.title3)
+                    .padding(.trailing)
             }
             .padding(.top)
-           
-            if let arquivo = arquivoAnexado{
-                HStack{
-                    Text(arquivo)
-                        .foregroundColor(.black)
-                    Spacer()
-                    Image(systemName: "doc.on.doc")
-                        .foregroundColor(.black)
-                }
-                .padding()
-                .background(Color.purple.opacity(0.4))
-                .cornerRadius(8)
-                .padding(.horizontal)
-            } else {
-                HStack(alignment: .center){
-                    Image(systemName: "paperclip")
-                        .foregroundColor(.black)
-                    Text("Anexar")
-                        .foregroundColor(.black)
-                    
-                    Spacer()
-                    
-                }
-               
-                .padding()
-                .background(Color.purple.opacity(0.4))
-                .cornerRadius(8)
-                .padding(.horizontal, 21)
             
-            }
-            TextField("Tipo de arquivo (extensão, como: .pdf )", text: $tipoArquivo)
-                .padding(.horizontal)
-                
-            Button(action: {
-                           // Ação de adicionar objetivo
-                       }) {
-                           Text("Adicionar objetivo")
-                               .foregroundColor(.white)
-                               .frame(maxWidth: .infinity)
-                               .padding()
-                               .background(Color.cyan)
-                               .cornerRadius(8)
-                       }
-                       .padding(.horizontal)
-
-                       Spacer()
+            Text("Tipo de arquivo (extensão, como: .pdf )")
+                .multilineTextAlignment(.trailing)
+                .foregroundColor(.gray)
+                .padding(.leading, -70)
         }
+        // List com Section e usar Head para descever o tipode arquivo
         
-      }
+        
+        VStack{
+            ScrollView(.vertical){
+            ForEach(0..<9){ number in
+               
+                    Label("AP2-2022.2.pdf", systemImage: "doc.on.doc")
+                        .frame(maxWidth:.infinity, minHeight: 40)
+                        .background(.purple.opacity(0.4))
+                        .cornerRadius(8)
+                        .padding(.bottom, 5)
+                        .padding(.horizontal)
+                }
+            }.frame(maxHeight: 250)
+        }
+        .foregroundColor(.black)
+        .padding(5)
+        
+        //Botão anexar
+        Button(action: {
+            
+        }){
+            Label("Anexar arquivo", systemImage: "paperclip")
+                .foregroundColor(.black)
+                .font(.title3)
+                .frame(maxWidth: .infinity)
+                .padding([.bottom, .top], 20)
+                .background(Color.purple.opacity(0.4))
+                .cornerRadius(8)
+        }
+        .padding(.horizontal)
+       
+        
+        Button(action: {
+            // Ação de adicionar objetivo
+        }) {
+            Text("Adicionar objetivo")
+                .foregroundColor(.black)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.cyan)
+                .cornerRadius(8)
+        }
+        .padding()
+        
+       
+        
+    }
     func anexarArquivo(){
         arquivoAnexado = "AP2-2022.2.pdf"
     }
-      
+    
 }
 
 #Preview {
